34aaaf4506fc225bd0f737fa5725fe5e20955c45
masepe95 <marcosepe10@gmail.com>
2023-05-26 16:48:17 +0200

    requiring info added

--- js/script.js
+++ js/script.js
@@ -7,5 +7,8 @@ console.log(totalCost);
 // COSTO PER CHILOMETRO
 const ticketCostKM = 0.21.toFixed(2);
 
+// RICHIESTA INFORMAZIONI
+const distanceRequired = parseInt(prompt('Quanti chilometri vuoi percorrere?', 100));
+const userAge = parseInt(prompt('Quanti anni hai?', 17));
 
 
